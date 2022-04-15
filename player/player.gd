d8a227c816702808097220cbd6102f848a07b583
Joe <joe.zlonicky@gmail.com>
2022-04-15 11:22:29 -0700

    base project

---
+++ player/player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+export (float) var current_speed = 0.0
+
+
+func _physics_process(_delta: float) -> void:
+	var input := Vector2()
+	if Input.is_action_pressed("move_up"):
+		input += Vector2.UP
+	if Input.is_action_pressed("move_right"):
+		input += Vector2.RIGHT
+	if Input.is_action_pressed("move_down"):
+		input += Vector2.DOWN
+	if Input.is_action_pressed("move_left"):
+		input += Vector2.LEFT
+	
+	if input and not $JumpPlayer.is_playing():
+		$JumpPlayer.play("jump")
+	
+	var move: Vector2 = input.normalized() * current_speed
+	
+	if move.x:
+		$Sprite.scale.x = -sign(move.x) * abs($Sprite.scale.x)
+	
+	var _result: Vector2 = move_and_slide(move)
